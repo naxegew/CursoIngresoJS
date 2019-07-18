da4b1add5c716a1b154a3fa1da07879ef1ee8f73
naxegew <52472615+naxegew@users.noreply.github.com>
2019-07-17 22:15:18 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,21 @@
-function mostrar()
-{
+function mostrar() {
 
-	var contador=0;
-	var acumulador=0;
+	var contador = 1;
+	var acumulador = 0;
+	var numero = 0;
 
+	while (contador <= 5) {
+		numero = parseInt(prompt("ingrese el " + contador + "º numero"));
+		while (isNaN(numero)) {
+			alert("ingreso un dato no numerico");
+			numero = parseInt(prompt("ingrese el " + contador + "º numero"));
+		}
+		acumulador = + numero;
+		contador ++;
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	}
+	
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / 5;
 
 }//FIN DE LA FUNCIÓN
