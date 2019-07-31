695be278d97f9a7de1d9000ae103a8d262c90dda
naxegew <52472615+naxegew@users.noreply.github.com>
2019-07-31 04:30:21 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,7 +8,112 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+    var cantidad;
+    var marca;
+    var precio;
+    var precioConDescuento;
+    var precioFinal = 0;
+    var impuesto = 0;
+    var flag = 0;
+
+    //---------cantidad de lamparas ingresadas---------
+    cantidad = parseInt(document.getElementById("Cantidad").value);
+
+    precio = cantidad * 35;
+    marca = document.getElementById("Marca").value;
+
+    switch (cantidad) {
+        case 5:
+            if (marca == 'ArgentinaLuz') {
+                precioFinal = precio * 0.6
+            }
+            else {
+                precioFinal = precio * 0.7;
+            }
+            break;
+        case 4:
+            if (marca == 'ArgentinaLuz' || marca == 'FelipeLamparas') {
+                precioFinal = precio * 0.75;
+            }
+            else {
+                precioFinal = precio * 0.80;
+            }
+            break;
+        case 3:
+            if (marca == 'ArgentinaLuz') {
+                precioFinal = precio * 0.85;
+            }
+            else if (marca == 'FelipeLamparas') {
+                precioFinal = precio * 0.9;
+            }
+            else {
+                precioFinal = precio * 0.95;
+            }
+            break;
+
+        default:
+            precioFinal = precio * 0.5;
+            break;
+    }
+    //---------------final del switch---------------------
+    if (precioFinal >= 120) {
+        impuesto = precioFinal * 0.1;
+        precioFinal = precioFinal + impuesto;
+        flag = 1;
+    }
+    if (flag = 1) {
+        document.getElementById("precioDescuento").value = "$" + precioFinal + " Usted pago $" + impuesto + " de IIBB.";
+    }
+    else {
+        document.getElementById("precioDescuento").value = "$" + precioFinal + " Usted pago $" + impuesto + " de IIBB.";
+    }
+
+
 }
+
+/*
+if (cantidad >= 6) {
+    precioFinal = precio * 0.5;
+}
+else if (cantidad == 5) {
+    if (marca == 'ArgentinaLuz') {
+
+        precioFinal = precio * 0.6;
+    }
+    else {
+
+        preciofinal = precio * 0.7;
+    }
+}
+else if (cantidad == 4) {
+    if (marca == 'ArgentinaLuz' || marca == 'FelipeLamparas') {
+        precioFinal = precio * 0.75;
+    }
+    else {
+        precioFinal = precio * 0.80;
+    }
+
+}
+else if (cantidad == 3) {
+    if (marca == 'ArgentinaLuz') {
+        precioFinal = precio * 0.85;
+    }
+    else if (marca == 'FelipeLamparas') {
+        precioFinal = precio * 0.9;
+    }
+    else {
+        precioFinal = precio * 0.95;
+    }
+}
+if (precioFinal >= 120) {
+    impuesto = precioFinal * 0.1;
+    precioFinal = precioFinal + impuesto;
+    flag = 1;
+}
+if (flag = 1) {
+    document.getElementById("precioDescuento").value = "$" + precioFinal + " Usted pago $" + impuesto + " de IIBB.";
+}
+else {
+    document.getElementById("precioDescuento").value = precioFinal;
+}*/
